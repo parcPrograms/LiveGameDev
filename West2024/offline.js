5c518d9aec8af3683174f05a6b76017813682fd7
parcPrograms <parcwashere@gmail.com>
2024-08-31 22:58:00 -0700

    west24 project

---
+++ West2024/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1725169271,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bird-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/background-sheet0.png",
+		"images/bottompipe-sheet0.png",
+		"images/toppipe-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/music_loop.m4a",
+		"media/music_loop.ogg",
+		"media/sfx_game_over.m4a",
+		"media/sfx_game_over.ogg",
+		"media/sfx_rhino.m4a",
+		"media/sfx_rhino.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
