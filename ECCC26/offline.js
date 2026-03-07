ac2a8b040e3cd966660900683bb486856cd8924c
parcPrograms <parcwashere@gmail.com>
2026-03-06 20:23:49 -0800

    eccc26

---
+++ ECCC26/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1772857256,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/platform-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/sprite3-sheet0.png",
+		"media/death.m4a",
+		"media/death.ogg",
+		"media/jump.m4a",
+		"media/jump.ogg",
+		"media/music.m4a",
+		"media/music.ogg",
+		"media/pickup.m4a",
+		"media/pickup.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
