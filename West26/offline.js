ac04a82d9f0e8519d418d24482c74680509b2901
parcPrograms <parcwashere@gmail.com>
2026-09-04 23:22:25 -0700

    add west 26

---
+++ West26/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1788588989,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite2-sheet2.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/tiledbackground2.png",
+		"media/collectable wife 1.m4a",
+		"media/collectable wife 1.ogg",
+		"media/collectable wife 2.m4a",
+		"media/collectable wife 2.ogg",
+		"media/collectable wife 3.m4a",
+		"media/collectable wife 3.ogg",
+		"media/wheres my wife.m4a",
+		"media/wheres my wife.ogg",
+		"media/restraining order.m4a",
+		"media/restraining order.ogg"
+	]
+}
